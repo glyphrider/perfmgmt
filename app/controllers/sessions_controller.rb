743256fb0458596d7f2c7600ba259152ad2407c9
Brian H. Ward <glyphrider@gmail.com>
2019-02-11 17:18:22 -0500

    bug with login restriction; now fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
-require 'octokit'
-# sessions_controller.rb
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_login(auth[:provider], auth[:extra][:raw_info][:login])
