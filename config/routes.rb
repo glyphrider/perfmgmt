b780d1209101ea44ead8cae8021ce3fecef6fdeb
Brian H. Ward <glyphrider@gmail.com>
2019-07-17 13:47:22 -0400

    updated app to ruby 2.6.3 and rails 5.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
-  get "/health_check" => 'sessions#index'
-  get "/auth/:provider/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
-  resources :users do
-    resources :meetings
-  end
-  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
